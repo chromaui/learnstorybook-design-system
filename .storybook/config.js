5b712084bc614943e56bd26d16e932419597ce9d
Tom Coleman <tom@thesnail.org>
2019-09-02 15:52:34 +1000

    Added addon-a11y

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
 import 'storybook-chromatic';
 
 import { GlobalStyle } from '../src/shared/global';
 
+addDecorator(withA11y);
 addDecorator(story => (
   <>
     <GlobalStyle />
