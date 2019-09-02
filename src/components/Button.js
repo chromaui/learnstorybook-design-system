a4d21376631c1e593b4b09f048ebe4b143a801d8
Tom Coleman <tom@thesnail.org>
2019-09-02 14:24:16 +1000

    make Button pop

--- src/components/Button.js
+++ src/components/Button.js
@@ -33,7 +33,9 @@ const SIZES = {
 };
 
 const StyledButton = styled.button`
-  border: 0;
+  border: 10px solid red;
+  font-size: 20px;
+  
   border-radius: 3em;
   cursor: pointer;
   display: inline-block;
@@ -52,7 +54,6 @@ const StyledButton = styled.button`
   background: transparent;
 
 
-  font-size: ${props => (props.size === SIZES.SMALL ? typography.size.s1 : typography.size.s2)}px;
   font-weight: ${typography.weight.extrabold};
   line-height: 1;
 
