59ffd772217420a5de674838288c3f81c4a6ea49
Tom Coleman <tom@thesnail.org>
2019-09-05 14:47:02 +1000

    Made a start on creating MDX file by importing from CSF
    Some issues here: https://docs.google.com/document/d/1WT81pH9V1aF_uWTEXcqXxsy09POqrmJ5ChH-HEZjGxo/edit#

---
+++ src/components/Avatar.stories.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { withKnobs, select, boolean } from '@storybook/addon-knobs';
+
+import { Avatar } from './Avatar';
+
+export const large = () => (
+  <div>
+    <Avatar loading size="large" />
+    <Avatar size="large" username="Tom Coleman" />
+    <Avatar
+      size="large"
+      username="Tom Coleman"
+      src="https://avatars2.githubusercontent.com/u/132554"
+    />
+  </div>
+);
+
+export const medium = () => (
+  <div>
+    <Avatar loading />
+    <Avatar username="Tom Coleman" />
+    <Avatar username="Tom Coleman" src="https://avatars2.githubusercontent.com/u/132554" />
+  </div>
+);
+
+export const small = () => (
+  <div>
+    <Avatar loading size="small" />
+    <Avatar size="small" username="Dominic Nguyen" />
+    <Avatar
+      size="small"
+      username="Dominic Nguyen"
+      src="https://avatars2.githubusercontent.com/u/263385"
+    />
+  </div>
+);
+
+export const tiny = () => (
+  <div>
+    <Avatar loading size="tiny" />
+    <Avatar size="tiny" username="Dominic Nguyen" />
+    <Avatar
+      size="tiny"
+      username="Dominic Nguyen"
+      src="https://avatars2.githubusercontent.com/u/263385"
+    />
+  </div>
+);
+
+export const knobs = () => (
+  <Avatar
+    loading={boolean('Loading')}
+    size={select('Size', ['tiny', 'small', 'medium', 'large'])}
+    username="Dominic Nguyen"
+    src="https://avatars2.githubusercontent.com/u/263385"
+  />
+);
+
+knobs.story = {
+  decorators: [withKnobs],
+};
