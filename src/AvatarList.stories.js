2775eabb4424c9f2467d8a54ef1c3c94228786bb
Tom Coleman <tom@thesnail.org>
2019-09-06 15:06:19 +1000

    Added basic AvatarList and story

---
+++ src/AvatarList.stories.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { AvatarList } from './AvatarList';
+
+export const users = [
+  {
+    id: '1',
+    name: 'Dominic Nguyen',
+    avatarUrl: 'https://avatars2.githubusercontent.com/u/263385',
+  },
+  {
+    id: '2',
+    name: 'Tom Coleman',
+    avatarUrl: 'https://avatars2.githubusercontent.com/u/132554',
+  },
+  {
+    id: '3',
+    name: 'Zoltan Olah',
+    avatarUrl: 'https://avatars0.githubusercontent.com/u/81672',
+  },
+  {
+    id: '4',
+    name: 'Tim Hingston',
+    avatarUrl: 'https://avatars3.githubusercontent.com/u/1831709',
+  },
+];
+
+export default {
+  title: 'Design System|AvatarList',
+  component: AvatarList,
+  excludeStories: ['users'],
+};
+
+export const short = () => <AvatarList users={users.slice(0, 2)} />;
