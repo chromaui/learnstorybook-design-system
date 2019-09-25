03289f453a23b4ceac6501d8d4020bbb40565770
Tom Coleman <tom@thesnail.org>
2019-09-25 13:17:51 +1000

    Add global styles

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,14 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+
+import { GlobalStyle } from '../src/shared/global';
+
+addDecorator(story => (
+  <>
+    <GlobalStyle />
+    {story()}
+  </>
+));
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src', true, /\.stories\.js$/), module);
--- src/shared/global.js
+++ src/shared/global.js
@@ -1,6 +1,8 @@
 import { createGlobalStyle, css } from 'styled-components';
 import { color, typography } from './styles';
 
+export const fontUrl = 'https://fonts.googleapis.com/css?family=Nunito+Sans:400,700,800,900';
+
 export const bodyStyles = css`
   font-family: ${typography.type.primary};
   font-size: ${typography.size.s3}px;
@@ -112,13 +114,7 @@ export const bodyStyles = css`
 `;
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Nunito+Sans:400,700,800,900');
-
-  body {
-    ${bodyStyles}
-
-    margin: 0;
-    overflow-y: auto;
-    overflow-x: hidden;
-  }
+ body {
+   ${bodyStyles}
+ }
 `;
