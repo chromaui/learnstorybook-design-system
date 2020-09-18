4211d5e095cba4524de9b57df2045e5a4b379344
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2020-09-18 19:42:41 +0100

    Add Controls example

--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -63,3 +63,13 @@ export const Large = (args) => (
     />
   </div>
 );
+
+const Template = (args) => <Avatar {...args} />;
+
+export const Controls = Template.bind({});
+Controls.args = {
+  loading: false,
+  size: "tiny",
+  username: "Dominic Nguyen",
+  src: "https://avatars2.githubusercontent.com/u/263385",
+};
