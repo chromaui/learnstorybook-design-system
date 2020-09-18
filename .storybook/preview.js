95d7ae772c4d562f3beab329659a1b53ba02f9f6
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2020-09-18 20:43:52 +0100

    Setup Storybook addon-a11y

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -13,4 +13,11 @@ export const decorators = [
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  // Storybook a11y addon configuration
+  a11y: {
+    // the target DOM element
+    element: '#root',
+    // sets the execution mode for the addon
+    manual: false,
+  },
 }
