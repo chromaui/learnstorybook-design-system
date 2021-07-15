116ee3bc392e7d325dac826478316121af558151
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-07-15 19:06:36 +0100

    Update README and add entrypoint

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import * as styles from "./shared/styles";
+import * as global from "./shared/global";
+import * as animation from "./shared/animation";
+import * as icons from "./shared/icons";
+
+export { styles, global, animation, icons };
+
+export * from "./Avatar";
+export * from "./Badge";
+export * from "./Button";
+export * from "./Icon";
+export * from "./Link";
