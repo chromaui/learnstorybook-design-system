f1624c73b6f0b7fd25b6eca0ab8f9899ed42e5ed
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-08-15 19:46:01 +0100

    Setup Storybook test runner

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,13 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+  features: {
+    interactionsDebugger: true,
+  },
+};
