7accaaedceea0602c34f5ccd7f8d416cdaf51f62
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-08-15 18:14:41 +0100

    Setup Storybook addon-a11y

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-a11y",
   ],
   framework: "@storybook/react",
   features: {
