5aa78f3d873eddf8199f44b73d16cf689a405f3e
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-06-15 14:57:36 +0100

    Tutorial initialized

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import * as styles from './shared/styles';
+import * as animation from './shared/animation';
+import * as icons from './shared/icons';
+import * as global from './shared/global';
+
+export { styles, animation, icons, global };
+
+export * from './Avatar';
+export * from './Badge';
+export * from './Button';
+export * from './Icon';
+export * from './Link';
+export * from './LinkWrapper';
