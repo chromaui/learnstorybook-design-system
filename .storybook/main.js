30853f53a1d41a2d44000eb4a0370704f754b0b8
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-06-15 21:06:56 +0100

    Added coverage testing

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ const config = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-a11y',
+    '@storybook/addon-coverage',
   ],
   framework: {
     name: '@storybook/react-vite',
