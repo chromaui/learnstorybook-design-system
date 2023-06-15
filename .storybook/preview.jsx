0c5fda0629adfc64bf30140aaeb63da1f844eaac
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-06-15 15:29:03 +0100

    Global styles configured

--- .storybook/preview.jsx
+++ .storybook/preview.jsx
@@ -1,5 +1,17 @@
+import { Global } from '@emotion/react';
+
+import { GlobalStyle } from '../src/shared/global';
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
+  decorators: [
+    (Story) => (
+      <>
+        <Global styles={GlobalStyle} />
+        <Story />
+      </>
+    ),
+  ],
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
