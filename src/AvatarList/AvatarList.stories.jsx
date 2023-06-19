bd9a7647bfa61717c2388153955756e1591227de
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-06-19 21:06:57 +0100

    AvatarList initial state

---
+++ src/AvatarList/AvatarList.stories.jsx
@@ -0,0 +1,24 @@
+import { AvatarList } from './AvatarList';
+
+export default {
+  title: 'Design System/AvatarList',
+  component: AvatarList,
+  tags: ['autodocs'],
+};
+
+export const Short = {
+  args: {
+    users: [
+      {
+        id: '1',
+        name: 'Dominic Nguyen',
+        avatarUrl: 'https://avatars2.githubusercontent.com/u/263385',
+      },
+      {
+        id: '2',
+        name: 'Tom Coleman',
+        avatarUrl: 'https://avatars2.githubusercontent.com/u/132554',
+      },
+    ],
+  },
+};
