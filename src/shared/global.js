2fc64957570c37e7eb56092ba133f0ca12d1f901
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-11-07 18:19:11 +0000

    Global styles configured

--- src/shared/global.js
+++ src/shared/global.js
@@ -1,6 +1,9 @@
 import { css } from '@emotion/react';
 import { color, typography } from './styles';
 
+export const fontUrl =
+  'https://fonts.googleapis.com/css?family=Nunito+Sans:400,700,800,900';
+
 export const bodyStyles = css`
   font-family: ${typography.type.primary};
   font-size: ${typography.size.s3}px;
