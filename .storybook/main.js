3f630df8fd362f51c64fdb7a69c5166dd696ecbf
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-12-21 20:11:18 +0000

    Setup Storybook addon-a11y

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,7 @@ const config = {
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/react-vite',
