7fc549b485bfc96365efe6c4cb2b918325f1a245
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-12-21 20:07:07 +0000

    Added Interaction story

--- src/Button/Button.stories.jsx
+++ src/Button/Button.stories.jsx
@@ -4,6 +4,7 @@ import { Button } from './Button';
 import { Icon } from '../Icon/Icon';
 import { StoryLinkWrapper } from '../LinkWrapper';
 
+import { expect, userEvent, within } from '@storybook/test';
 
 const CustomButton = styled.button`
   border: 1px solid green;
@@ -327,3 +328,20 @@ export const AnchorWrapper = {
   ),
 };
 
+export const WithInteractions = {
+  args: {
+    appearance: 'primary',
+    href: 'http://storybook.js.org',
+    ButtonWrapper: StoryLinkWrapper,
+    children: 'Button',
+  },
+  play: async ({ canvasElement }) => {
+    // Assigns canvas to the component root element
+    const canvas = within(canvasElement);
+    await userEvent.click(canvas.getByRole('link'));
+    expect(canvas.getByRole('link')).toHaveAttribute(
+      'href',
+      'http://storybook.js.org',
+    );
+  },
+};
