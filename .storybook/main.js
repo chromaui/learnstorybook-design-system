ace28679d82e1541d3e65472aed264f9cac02686
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2023-12-23 21:10:05 +0000

    Introduction page included

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,10 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    '../src/Intro.mdx',
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
